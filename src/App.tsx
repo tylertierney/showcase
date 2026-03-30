c196c7a037fcd644097b3bb6c4f242d15972d020
Tyler Tierney <tytierney@yahoo.com>
2026-03-30 16:18:24 -0400

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center">
+      {/* <h1 className="text-3xl font-bold underline">Hellooooo</h1> */}
+      <div
+        style={{ height: '200px', width: '200px', background: 'orange' }}
+      ></div>
+      <div
+        style={{ height: '100px', width: '100px', background: 'lightblue' }}
+      ></div>
+    </div>
+  )
+}
+
+export default App
